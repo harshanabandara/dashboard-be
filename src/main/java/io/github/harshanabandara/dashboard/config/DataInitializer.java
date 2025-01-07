bbf60dc7d247e6846991c5fd951be28b848bde7a
Harshana <e16049@eng.pdn.ac.lk>
2025-01-07 16:00:56 +0530

    Created init data for test users.

---
+++ src/main/java/io/github/harshanabandara/dashboard/config/DataInitializer.java
@@ -0,0 +1,57 @@
+package io.github.harshanabandara.dashboard.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import io.github.harshanabandara.dashboard.model.Credential;
+import io.github.harshanabandara.dashboard.model.User;
+import io.github.harshanabandara.dashboard.repository.CredentialRepository;
+import io.github.harshanabandara.dashboard.repository.UserRepository;
+import io.github.harshanabandara.dashboard.util.PasswordUtil;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final CredentialRepository credentialRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public DataInitializer(CredentialRepository credentialRepository, UserRepository userRepository) {
+        this.credentialRepository = credentialRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (userRepository.count() == 0) {
+            User admin = new User();
+            admin.setFirstName("Harshana");
+            admin.setLastName("Bandara");
+            userRepository.save(admin);
+            User user = new User();
+            user.setFirstName("John");
+            user.setLastName("Doe");
+            userRepository.save(user);
+        }
+        if (credentialRepository.count() == 0) {
+            Credential adminCredential = new Credential();
+            Credential userCredential = new Credential();
+
+            adminCredential.setUsername("haulmatic");
+            adminCredential.setPassword(PasswordUtil.digestPassword("123456"));
+            adminCredential.setRole("ADMIN");
+            adminCredential.setUserId(1);
+            credentialRepository.save(adminCredential);
+
+            userCredential.setUsername("default");
+            userCredential.setPassword(PasswordUtil.digestPassword("123456"));
+            userCredential.setRole("user");
+            userCredential.setUserId(2);
+            credentialRepository.save(userCredential);
+        }
+
+    }
+
+}
--- src/main/java/io/github/harshanabandara/dashboard/config/WebSecurityConfig.java
+++ src/main/java/io/github/harshanabandara/dashboard/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +12,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // TODO: This is here because I need a way to log into h2 console.
         http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
         return http.build();
     }
