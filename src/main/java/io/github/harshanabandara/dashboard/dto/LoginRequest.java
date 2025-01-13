bdf7d2012f19e0a1fe39c1a15fb32dbb3c2c171b
Harshana <e16049@eng.pdn.ac.lk>
2025-01-13 18:26:00 +0530

    removed broken annotation. added debug logs.

--- src/main/java/io/github/harshanabandara/dashboard/dto/LoginRequest.java
+++ src/main/java/io/github/harshanabandara/dashboard/dto/LoginRequest.java
@@ -1,7 +1,5 @@
 package io.github.harshanabandara.dashboard.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 public class LoginRequest {
     private String username;
     private String password;
